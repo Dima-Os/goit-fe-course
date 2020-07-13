b126cf74d1f5eb42b9ef36271932755be9d1f51c
Dima_Os <osipovdima1989@gmail.com>
2020-07-13 19:45:44 +0300

    debuged some tasks

--- javascript/module-02/js/task-04.js
+++ javascript/module-02/js/task-04.js
@@ -1,12 +1,29 @@
-"use strict";
+'use strict';
 const formatString = function (string) {
-  string.length <= 40 ? console.log(string) : console.log(string + " ...");
+  if (string.length <= 40) {
+    return string;
+  }
+  return string.slice(0, 40) + ' ...';
 };
-console.log(formatString("Curabitur ligula sapien, tincidunt non."));
-console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
-console.log(formatString("Curabitur ligula sapien."));
-console.log(
-  formatString(
-    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
-  )
-);
+const cutStringFirstRef = document.querySelector('.cut-string-first');
+cutStringFirstRef.addEventListener('click', () => {
+  console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+});
+const cutStringSecondRef = document.querySelector('.cut-string-second');
+cutStringSecondRef.addEventListener('click', () => {
+  console.log(
+    formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'),
+  );
+});
+const cutStringThirdRef = document.querySelector('.cut-string-third');
+cutStringThirdRef.addEventListener('click', () => {
+  console.log(formatString('Curabitur ligula sapien.'));
+});
+const cutStringFourthRef = document.querySelector('.cut-string-fourth');
+cutStringFourthRef.addEventListener('click', () => {
+  console.log(
+    formatString(
+      'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+    ),
+  );
+});
