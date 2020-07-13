c0f04aa6727f9f1b60e1c2aba31261031c68a697
Dima_Os <osipovdima1989@gmail.com>
2020-07-13 16:50:44 +0300

    added task-04

--- javascript/module-02/js/task-04.js
+++ javascript/module-02/js/task-04.js
@@ -1,24 +1,12 @@
-'use strict';
-let credits = 23580;
-let totalPrice = 0;
-const pricePerDroid = 3000;
-const buyButtonRef = document.querySelector('.buy-button');
-buyButtonRef.addEventListener('click', () => {
-  const imputtedAmount = prompt('Введите количество дроидов');
-  if (imputtedAmount === null) {
-    console.log('Отменено пользователем!');
-  } else if (Number.isNaN(Number(imputtedAmount)) || imputtedAmount <= 0) {
-    console.log('Введено не корректное число!');
-  } else {
-    totalPrice = Number(imputtedAmount) * pricePerDroid;
-    if (totalPrice > credits) {
-      console.log('Недостаточно средств на счету!');
-    } else {
-      console.log(
-        `Вы купили ${Number(imputtedAmount)} дроидов, на счету осталось ${
-          credits - totalPrice
-        } кредитов.)`,
-      );
-    }
-  }
-});
+"use strict";
+const formatString = function (string) {
+  string.length <= 40 ? console.log(string) : console.log(string + " ...");
+};
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+console.log(formatString("Curabitur ligula sapien."));
+console.log(
+  formatString(
+    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+  )
+);
