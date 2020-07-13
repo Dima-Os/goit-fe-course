60bfd1a1d2e93926c33c7514f429b4ea91215af0
Dima_Os <osipovdima1989@gmail.com>
2020-07-13 16:07:53 +0300

    added task-03

--- javascript/module-02/js/task-03.js
+++ javascript/module-02/js/task-03.js
@@ -1,15 +1,25 @@
-'use strict';
-const ADMIN_PASSWORD = 'jqueryismy';
-const authorizationRef = document.querySelector('.authorization');
-let message;
-authorizationRef.addEventListener('click', () => {
-  const userInput = prompt('Введите пароль:');
-  if (userInput === null) {
-    message = 'Отменено пользователем!';
-  } else if (userInput === ADMIN_PASSWORD) {
-    message = 'Добро пожаловать!';
-  } else {
-    message = 'Доступ запрещен, неверный пароль!';
+"use strict";
+const findLongestWord = function (string) {
+  let LongestWord = string.split(" ")[0];
+  for (let i = 1; i < string.split(" ").length; i += 1) {
+    if (string.split(" ")[i].length > LongestWord.length) {
+      LongestWord = string.split(" ")[i];
+    }
   }
-  alert(message);
+  return LongestWord;
+};
+/*Just to separate tasks of homework*/
+const longestWordFirstRef = document.querySelector(".longest-word-first");
+longestWordFirstRef.addEventListener("click", () => {
+  console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+});
+
+const longestWordSecondRef = document.querySelector(".longest-word-second");
+longestWordSecondRef.addEventListener("click", () => {
+  console.log(findLongestWord("Google do a roll"));
+});
+
+const longestWordThirdRef = document.querySelector(".longest-word-third");
+longestWordThirdRef.addEventListener("click", () => {
+  console.log(findLongestWord("May the force be with you"));
 });
