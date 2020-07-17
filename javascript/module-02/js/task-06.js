6bcc84bc9f717d46858818979d337933174fe50b
Dima_Os <osipovdima1989@gmail.com>
2020-07-18 02:06:34 +0300

    added some css

--- javascript/module-02/js/task-06.js
+++ javascript/module-02/js/task-06.js
@@ -9,15 +9,14 @@ startInputButtonRef.addEventListener('click', () => {
     input = prompt('Введите число:');
     if (Number.isNaN(+input)) {
       alert('Было введено не число, попробуйте еще раз!');
+    } else if (input !== null) {
+      numbers.push(input);
     }
-    numbers.push(input);
   } while (input !== null);
-
   if (numbers.length > 0) {
     for (const element of numbers) {
       total += +element;
     }
-
     console.log(`Общая сумма чисел равна: ${total} .`);
   }
 });
