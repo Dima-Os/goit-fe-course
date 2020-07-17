e5332bb7212ab33ea8e2633517658284cfd81f94
Dima_Os <osipovdima1989@gmail.com>
2020-07-18 00:20:03 +0300

    debuged task-06-07

--- javascript/module-02/js/task-06.js
+++ javascript/module-02/js/task-06.js
@@ -1,25 +1,23 @@
-"use strict";
+'use strict';
 let input;
 const numbers = [];
 let total = 0;
-const SummOfArray = function (inputedArray) {
-  for (const element of inputedArray) {
-    total += +element;
-  }
-  return total;
-};
-const startInputButtonRef = document.querySelector(".start-input-button");
-startInputButtonRef.addEventListener("click", () => {
+
+const startInputButtonRef = document.querySelector('.start-input-button');
+startInputButtonRef.addEventListener('click', () => {
   do {
-    input = prompt("Введите число:");
-    if (Number.isNaN(Number(input))) {
-      alert("Было введено не число, попробуйте еще раз!");
-    } else if (input === null) {
-      break;
-    } else {
-      numbers.push(input);
+    input = prompt('Введите число:');
+    if (Number.isNaN(+input)) {
+      alert('Было введено не число, попробуйте еще раз!');
     }
+    numbers.push(input);
   } while (input !== null);
 
-  console.log(SummOfArray(numbers));
+  if (numbers.length > 0) {
+    for (const element of numbers) {
+      total += +element;
+    }
+
+    console.log(`Общая сумма чисел равна: ${total} .`);
+  }
 });
--- javascript/module-02/js/task-07.js
+++ javascript/module-02/js/task-07.js
@@ -1,26 +1,24 @@
-"use strict";
+'use strict';
 export const logins = [
-  "Mango",
-  "robotGoogles",
-  "Poly",
-  "Aj4x1sBozz",
-  "qwerty123",
+  'Mango',
+  'robotGoogles',
+  'Poly',
+  'Aj4x1sBozz',
+  'qwerty123',
 ];
 export const isLoginValid = function (login) {
   return login.length >= 4 && login.length <= 16;
 };
 export const isLoginUnique = function (allLogins, login) {
-  return !allLogins.includes(login);
+  return allLogins.includes(login);
 };
 export const addLogin = function (allLogins, login) {
-  if (isLoginValid(login) && isLoginUnique(allLogins, login)) {
-    logins.push(login);
-    return "Логин успешно добавлен!";
+  if (isLoginUnique(allLogins, login)) {
+    return 'Такой логин уже используется!';
   }
-  if (!isLoginValid(login)) {
-    return "Ошибка! Логин должен быть от 4 до 16 символов";
-  }
-  if (!isLoginUnique(allLogins, login)) {
-    return "Такой логин уже используется!";
+  if (isLoginValid(login)) {
+    return 'Логин успешно добавлен!';
+  } else {
+    return 'Ошибка! Логин должен быть от 4 до 16 символов!';
   }
 };
