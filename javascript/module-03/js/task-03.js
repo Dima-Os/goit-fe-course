d41f07f3d167da269f4546dcd57f853d68e712f6
Dima_Os <osipovdima1989@gmail.com>
2020-07-20 02:13:35 +0300

    added task-03

---
+++ javascript/module-03/js/task-03.js
@@ -0,0 +1,16 @@
+'use strict';
+export const findBestEmployee = function (employees) {
+  for (const name in employees) {
+  }
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */ console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
