f5cf6467b4efcc44bf5b8263ea7641bdbe83f6a1
Dima_Os <osipovdima1989@gmail.com>
2020-07-22 23:24:51 +0300

    ADDED TASK -07

--- javascript/module-03/js/task-07.js
+++ javascript/module-03/js/task-07.js
@@ -3,18 +3,56 @@ const Transaction = {
   DEPOSIT: 'deposit',
   WITHDRAW: 'withdraw',
 };
-
 const account = {
   balance: 0,
   transactions: [],
   createTransaction(amount, type) {
-    this.transactions.push({ AMOUNT: amount, TYPE: type, ID: 0 });
+    return {
+      amount: amount,
+      type: type,
+      id: this.transactions.length,
+    };
+  },
+  deposit(amount) {
+    const currentTransaction = this.createTransaction(
+      amount,
+      Transaction.DEPOSIT,
+    );
+    this.transactions.push(currentTransaction);
+    this.balance += amount;
+    return;
+  },
+  withdraw(amount) {
+    if (this.balance > amount) {
+      const currentTransaction = this.createTransaction(
+        amount,
+        Transaction.DEPOSIT,
+      );
+      this.transactions.push(currentTransaction);
+      this.balance -= amount;
+      return;
+    }
+    return 'Fuck you!';
   },
 };
 
-account.createTransaction(300, 'deposit');
 console.log(account);
-console.log(account.transactions);
-account.createTransaction(600, 'deposit');
+console.log(account.balance);
+
+console.log(account.withdraw(2000));
+console.log(account.withdraw(10));
+console.log(account.withdraw(1));
+account.deposit(500);
+account.deposit(600);
+account.deposit(100);
+console.log(account.withdraw(10));
+console.log(account.withdraw(10));
 console.log(account);
-console.log(account.transactions);
+console.log(account.balance);
+console.log(account.withdraw(2000));
+console.log(account);
+console.log(account.withdraw(100));
+console.log(account.balance);
+console.log(account.withdraw(2000));
+console.log(account);
+console.log(account.balance);
