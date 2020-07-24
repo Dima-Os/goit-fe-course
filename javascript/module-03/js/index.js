61ccbf28b5ce5a86db35c11d06d215947dcdd610
Dima_Os <osipovdima1989@gmail.com>
2020-07-24 22:46:08 +0300

    debuged task-07

--- javascript/module-03/js/index.js
+++ javascript/module-03/js/index.js
@@ -1,40 +1,40 @@
-"use strict";
+'use strict';
 /*==========task-01=============*/
-import { user } from "./task-01.js";
-const runLogRef = document.querySelector(".run-log");
-runLogRef.addEventListener("click", () => {
+import { user } from './task-01.js';
+const runLogRef = document.querySelector('.run-log');
+runLogRef.addEventListener('click', () => {
   for (const key of Object.keys(user)) {
     console.log(`${key}: ${user[key]}`);
   }
 });
 /*==========task-02=============*/
-import { countProps } from "./task-02.js";
-const runFunctionRef = document.querySelector(".run-function");
-runFunctionRef.addEventListener("click", () => {
+import { countProps } from './task-02.js';
+const runFunctionRef = document.querySelector('.run-function');
+runFunctionRef.addEventListener('click', () => {
   console.log(countProps({}));
-  console.log(countProps({ name: "Mango", age: 2 }));
+  console.log(countProps({ name: 'Mango', age: 2 }));
   console.log(
-    countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })
+    countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }),
   );
 });
 /*==========task-03=============*/
-import { findBestEmployee } from "./task-03.js";
-const findBestEmployeeRef = document.querySelector(".find-best-employee");
-findBestEmployeeRef.addEventListener("click", () => {
+import { findBestEmployee } from './task-03.js';
+const findBestEmployeeRef = document.querySelector('.find-best-employee');
+findBestEmployeeRef.addEventListener('click', () => {
   console.log(
     findBestEmployee({
       ann: 29,
       david: 35,
       helen: 1,
       lorence: 99,
-    })
+    }),
   );
   console.log(
     findBestEmployee({
       poly: 12,
       mango: 17,
       ajax: 4,
-    })
+    }),
   );
 
   console.log(
@@ -43,13 +43,13 @@ findBestEmployeeRef.addEventListener("click", () => {
       david: 21,
       kiwi: 19,
       chelsy: 38,
-    })
+    }),
   );
 });
 /*==========task-04=============*/
-import { countTotalSalary } from "./task-04.js";
-const countTotalSalaryRef = document.querySelector(".count-total-salary");
-countTotalSalaryRef.addEventListener("click", () => {
+import { countTotalSalary } from './task-04.js';
+const countTotalSalaryRef = document.querySelector('.count-total-salary');
+countTotalSalaryRef.addEventListener('click', () => {
   console.log(countTotalSalary({})); // 0
 
   console.log(
@@ -57,7 +57,7 @@ countTotalSalaryRef.addEventListener("click", () => {
       mango: 100,
       poly: 150,
       alfred: 80,
-    })
+    }),
   );
 
   console.log(
@@ -65,42 +65,42 @@ countTotalSalaryRef.addEventListener("click", () => {
       kiwi: 200,
       lux: 50,
       chelsy: 150,
-    })
+    }),
   );
 });
 /*==========task-05=============*/
-import { getAllPropValues } from "./task-05.js";
-import { products } from "./task-05.js";
-const returnValuesArrayRef = document.querySelector(".return-values-array");
-returnValuesArrayRef.addEventListener("click", () => {
-  console.log(getAllPropValues(products, "name"));
-  console.log(getAllPropValues(products, "quantity"));
-  console.log(getAllPropValues(products, "category"));
+import { getAllPropValues } from './task-05.js';
+import { products } from './task-05.js';
+const returnValuesArrayRef = document.querySelector('.return-values-array');
+returnValuesArrayRef.addEventListener('click', () => {
+  console.log(getAllPropValues(products, 'name'));
+  console.log(getAllPropValues(products, 'quantity'));
+  console.log(getAllPropValues(products, 'category'));
 });
 /*==========task-06=============*/
 
-import { calculateTotalPrice } from "./task-06.js";
-const calculateTotalPriceRef = document.querySelector(".calculate-total-price");
-calculateTotalPriceRef.addEventListener("click", () => {
-  console.log(calculateTotalPrice(products, "Радар")); // 5200
-  console.log(calculateTotalPrice(products, "Дроид"));
+import { calculateTotalPrice } from './task-06.js';
+const calculateTotalPriceRef = document.querySelector('.calculate-total-price');
+calculateTotalPriceRef.addEventListener('click', () => {
+  console.log(calculateTotalPrice(products, 'Радар')); // 5200
+  console.log(calculateTotalPrice(products, 'Дроид'));
 });
 
 /*==========task-07=============*/
 /*A few functions just to test methods */
-import { Transaction } from "./task-07.js";
-import { account } from "./task-07.js";
-const runCabinet = document.querySelector(".run-cabinet");
-runCabinet.addEventListener("click", () => {
+import { Transaction } from './task-07.js';
+import { account } from './task-07.js';
+const runCabinet = document.querySelector('.run-cabinet');
+runCabinet.addEventListener('click', () => {
   account.deposit(500);
   account.deposit(600);
   account.deposit(100);
-  console.log(account.withdraw(10));
-  console.log(account.withdraw(10));
-  console.log(account.withdraw(2000));
-  console.log(account.withdraw(100));
+  account.withdraw(10);
+  account.withdraw(10);
+  account.withdraw(200);
+  account.withdraw(100);
   console.log(account.getBalance());
-  console.log(account.withdraw(2000));
+  account.withdraw(200);
   console.log(account.getBalance());
   console.log(account.getTransactionDetails(4));
   console.log(account.getTransactionTotal(Transaction.WITHDRAW));
