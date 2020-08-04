5e12c3dc787ab6605e12a08959abd23687615d75
Dima_Os <osipovdima1989@gmail.com>
2020-08-04 19:54:41 +0300

    added task-07

---
+++ javascript/module-05/js/task-04.js
@@ -0,0 +1,47 @@
+'use strict';
+class StringBuilder {
+  constructor(_value) {
+    this._value = _value;
+  }
+  get value() {
+    return this._value;
+  }
+  append(str) {
+    this._value += str;
+  }
+  prepend(str) {
+    this._value = str + this._value;
+  }
+  pad(str) {
+    this._value = str + this._value + str;
+  }
+}
+
+const builder = new StringBuilder('.');
+builder.append('^');
+console.log(builder.value);
+
+builder.prepend('^');
+console.log(builder.value);
+builder.pad('=');
+console.log(builder.value);
+
+/*Напиши класс StringBuilder. На вход он получает один параметр
+ - строку, которую записывает в свойство _value.
+
+Добавь классу следующий функционал:
+
+Геттер value - возвращает текущее значение поля _value
+Метод append(str) - получает парметр str (строку) и добавляет ее в конец _value
+Метод prepend(str) - получает парметр str (строку) и добавляет ее в начало value
+Метод pad(str) - получает парметр str (строку) и добавляет ее в начало и в конец _value
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='*/
