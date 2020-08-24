f85db36964487a83a55413d0cef2f3ca0f24c04b
Dima_Os <osipovdima1989@gmail.com>
2020-08-24 18:20:27 +0300

    added task-02

--- javascript/module-07/js/index.js
+++ javascript/module-07/js/index.js
@@ -3,11 +3,15 @@ import {
   categoriesRef,
   showCategories,
   showHeadingWithCount,
-} from "./task-01.js";
-const runlogRef = document.querySelector(".run-log");
+} from './task-01.js';
+const runlogRef = document.querySelector('.run-log');
 const runLog = () => {
   showCategories(categoriesRef);
   showHeadingWithCount(categoriesRef);
 };
-runlogRef.addEventListener("click", runLog);
+runlogRef.addEventListener('click', runLog);
 // ===========Task-02===========
+import { ingredients, galleryRef, createItem, createList } from './task-02.js';
+const runSecondLogRef = document.querySelector('.run-second-log');
+const runSecondLog = () => galleryRef.append(...createList(ingredients));
+runSecondLogRef.addEventListener('click', runSecondLog);
--- javascript/module-07/js/task-02.js
+++ javascript/module-07/js/task-02.js
@@ -1,19 +1,19 @@
-const ingredients = [
-  "Картошка",
-  "Грибы",
-  "Чеснок",
-  "Помидоры",
-  "Зелень",
-  "Приправы",
+export const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
 ];
-const galleryRef = document.querySelector("#gallery");
+export const galleryRef = document.querySelector('#gallery');
 
-const createItem = (listItem) => {
-  const createdIUtem = document.createElement("li");
+export const createItem = listItem => {
+  const createdIUtem = document.createElement('li');
   createdIUtem.textContent = listItem;
   return createdIUtem;
 };
 
-const createList = (list) => list.map((el) => createItem(el));
+export const createList = list => list.map(el => createItem(el));
 
 // const appendlist = galleryRef.append(...createList(ingredients));
