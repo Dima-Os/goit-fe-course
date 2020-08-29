6f58922b8652d0795be0f64a5d0de4acbbe0d5d8
Dima_Os <osipovdima1989@gmail.com>
2020-08-29 08:43:53 +0300

    debuged random color function

--- javascript/module-07/js/task-08.js
+++ javascript/module-07/js/task-08.js
@@ -1,41 +1,43 @@
-const boxesRef = document.querySelector("#boxes");
-const controlsRef = document.querySelector("#controls");
+const boxesRef = document.querySelector('#boxes');
+const controlsRef = document.querySelector('#controls');
 const userInputRef = document.querySelector('#controls > input[type="number"]');
 const renderBtnRef = document.querySelector(
-  '#controls > button[data-action="render"]'
+  '#controls > button[data-action="render"]',
 );
 const destroyBtnRef = document.querySelector(
-  '#controls > button[data-action="destroy"]'
+  '#controls > button[data-action="destroy"]',
 );
 
-let r,
-  g,
-  b = 0;
-const getRundomNum = () => parseInt(Math.random() * 255);
+function getRundomColor() {
+  const getRundomNum = () => parseInt(Math.random() * 255);
+  const r = getRundomNum();
+  const g = getRundomNum();
+  const b = getRundomNum();
+  return `rgb(${r},${g},${b})`;
+}
+
 function createBox(size) {
-  const box = document.createElement("div");
+  const box = document.createElement('div');
   box.setAttribute(
-    "style",
-    `width:${size}px; height:${size}px; background-color:rgb(${r},${g},${b})`
+    'style',
+    `width:${size}px; height:${size}px; background-color:${getRundomColor()}`,
   );
   return box;
 }
 
 function createBoxes(amount) {
-  let createdBoxes = document.createElement("div");
+  let createdBoxes = document.createElement('div');
   for (let i = 1; i <= amount; i += 1) {
-    r = getRundomNum();
-    g = getRundomNum();
-    b = getRundomNum();
     const size = 30 + i * 10;
     const createdBox = createBox(size);
     createdBoxes.append(createdBox);
   }
   boxesRef.append(createdBoxes);
-  userInputRef.value = "";
+  userInputRef.value = '';
 }
 function onRender(event) {
   if (event.target === renderBtnRef) createBoxes(+userInputRef.value);
-  else if (event.target === destroyBtnRef) boxesRef.innerHTML = "";
+  else if (event.target === destroyBtnRef) boxesRef.innerHTML = '';
 }
-controlsRef.addEventListener("click", onRender);
+
+controlsRef.addEventListener('click', onRender);
