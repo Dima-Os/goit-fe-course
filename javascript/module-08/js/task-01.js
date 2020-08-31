0e5172022bbde7ddd672fc9ff249cc426a5ccbd6
Dima_Os <osipovdima1989@gmail.com>
2020-08-31 22:41:39 +0300

    added task-01

--- javascript/module-08/js/task-01.js
+++ javascript/module-08/js/task-01.js
@@ -1,63 +1,89 @@
 const refs = {
-  gallery: document.querySelector(".gallery"),
-  lightbox: document.querySelector(".lightbox"),
-  lightboxImage: document.querySelector(".lightbox__image"),
+  gallery: document.querySelector('.gallery'),
+  lightbox: document.querySelector('.lightbox'),
+  lightboxImage: document.querySelector('.lightbox__image'),
   closeButton: document.querySelector('button[data-action="close-lightbox"]'),
 };
+let currentIndex = 0;
+import galleryItems from './gallery-items.js';
 
-import galleryItems from "./gallery-items.js";
-
-function createGalleryItem(element) {
-  const galleryItem = document.createElement("li");
-  galleryItem.classList.add("gallery__item");
-  const itemLink = document.createElement("a");
-  itemLink.classList.add("gallery__link");
+function createGalleryItem(element, index) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+  const itemLink = document.createElement('a');
+  itemLink.classList.add('gallery__link');
   galleryItem.appendChild(itemLink);
-  itemLink.setAttribute("href", `${element.original}`);
-  const galleryImage = document.createElement("img");
-  galleryImage.classList.add("gallery__image");
-  galleryImage.setAttribute("src", `${element.preview}`);
-  galleryImage.setAttribute("data-source", `${element.original}`);
-  galleryImage.setAttribute("alt", `${element.description}`);
+  itemLink.setAttribute('href', `${element.original}`);
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.setAttribute('src', `${element.preview}`);
+  galleryImage.setAttribute('data-source', `${element.original}`);
+  galleryImage.setAttribute('data-index', `${index}`);
+  galleryImage.setAttribute('alt', `${element.description}`);
   itemLink.appendChild(galleryImage);
   return galleryItem;
 }
 
 function createGalleryItems(elements) {
-  return elements.map((element) => createGalleryItem(element));
+  return elements.map((element, index) => createGalleryItem(element, index));
 }
 
 function renderGalleryItems() {
   refs.gallery.append(...createGalleryItems(galleryItems));
 }
 function escHendler(event) {
-  if (event.code === "Escape") {
+  if (event.code === 'Escape') {
     onCloseButton();
   }
 }
+
+function next() {
+  if (currentIndex > galleryItems.length - 2) return;
+  ++currentIndex;
+  console.log(currentIndex);
+}
+
+function previous() {
+  if (currentIndex < 1) return;
+  --currentIndex;
+  console.log(currentIndex);
+}
+
+function arrowHendler(event) {
+  if (event.code === 'ArrowRight') {
+    next();
+  } else if (event.code === 'ArrowLeft') {
+    previous();
+  }
+  refs.lightboxImage.src = galleryItems[currentIndex].original;
+}
+
 function onClickImage(event) {
   event.preventDefault();
-  window.addEventListener("keydown", escHendler);
-  if (event.target.nodeName === "IMG") {
-    refs.lightbox.classList.add("is-open");
+  window.addEventListener('keydown', escHendler);
+  window.addEventListener('keydown', arrowHendler);
+  if (event.target.nodeName === 'IMG') {
+    refs.lightbox.classList.add('is-open');
     refs.lightboxImage.src = event.target.dataset.source;
     refs.lightboxImage.alt = event.target.alt;
   }
+  currentIndex = +event.target.dataset.index;
 }
 
 function onCloseButton() {
-  refs.lightbox.classList.remove("is-open");
-  refs.lightboxImage.src = "";
-  refs.lightboxImage.alt = "";
-  window.removeEventListener("keydown", escHendler);
+  refs.lightbox.classList.remove('is-open');
+  refs.lightboxImage.src = '';
+  refs.lightboxImage.alt = '';
+  window.removeEventListener('keydown', escHendler);
+  window.removeEventListener('keydown', arrowHendler);
 }
 
 function onLightboxClick(event) {
-  if (event.target.nodeName !== "IMG") onCloseButton();
+  if (event.target.nodeName !== 'IMG') onCloseButton();
 }
 
-refs.closeButton.addEventListener("click", onCloseButton);
-refs.gallery.addEventListener("click", onClickImage);
-refs.lightbox.addEventListener("click", onLightboxClick);
+refs.closeButton.addEventListener('click', onCloseButton);
+refs.gallery.addEventListener('click', onClickImage);
+refs.lightbox.addEventListener('click', onLightboxClick);
 
 renderGalleryItems();
